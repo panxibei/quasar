7bf893b0b61c2ffbb5c5765cb08ffbdbe359bb65
panxibei <panxibei@yandex.com>
2020-02-10 15:31:37 +0800

    quasar

--- resources/views/test/quasar.blade.php
+++ resources/views/test/quasar.blade.php
@@ -19,6 +19,33 @@
 }
 </style>
 
+<style type="text/css">
+.my-sticky-header-table {
+  /* max height is important */
+  /* this is when the loading indicator appears */
+}
+.my-sticky-header-table .q-table__middle {
+  max-height: 200px;
+}
+.my-sticky-header-table .q-table__top,
+.my-sticky-header-table .q-table__bottom,
+.my-sticky-header-table thead tr:first-child th {
+  /* bg color is important for th; just specify one */
+  background-color: #c1f4cd;
+}
+.my-sticky-header-table thead tr th {
+  position: sticky;
+  z-index: 1;
+}
+.my-sticky-header-table thead tr:first-child th {
+  top: 0;
+}
+.my-sticky-header-table.q-table--loading thead tr:last-child th {
+  /* height of all previous header rows */
+  top: 48px;
+}
+</style>
+
 </head>
 <body>
 
@@ -248,6 +275,68 @@
 
       </div>
 
+      <div class="q-pa-md">
+        <q-table
+          class="my-sticky-header-table"
+          title="Treats"
+          :data="data"
+          :columns="columns"
+          row-key="name"
+          flat
+          bordered
+          :separator="separator"
+          :selected-rows-label="getSelectedString"
+          selection="multiple"
+          :selected.sync="tableselected"
+          :visible-columns="visibleColumns"
+        >
+        
+        <template v-slot:top="props">
+          <div class="col-2 q-table__title">Treats</div>
+
+          <q-space></q-space>
+
+          <div v-if="$q.screen.gt.xs" class="col">
+            <q-toggle v-model="visibleColumns" val="calories" label="Calories"></q-toggle>
+            <q-toggle v-model="visibleColumns" val="fat" label="Fat"></q-toggle>
+            <q-toggle v-model="visibleColumns" val="carbs" label="Carbs"></q-toggle>
+            <q-toggle v-model="visibleColumns" val="protein" label="Protein"></q-toggle>
+            <q-toggle v-model="visibleColumns" val="sodium" label="Sodium"></q-toggle>
+            <q-toggle v-model="visibleColumns" val="calcium" label="Calcium"></q-toggle>
+            <q-toggle v-model="visibleColumns" val="iron" label="Iron"></q-toggle>
+          </div>
+          <q-select
+            v-else
+            v-model="visibleColumns"
+            multiple
+            borderless
+            dense
+            options-dense
+            :display-value="$q.lang.table.columns"
+            emit-value
+            map-options
+            :options="columns"
+            option-value="name"
+            style="min-width: 150px"
+          ></q-select>
+
+          <q-btn
+            flat round dense
+            :icon="props.inFullscreen ? 'fullscreen_exit' : 'fullscreen'"
+            @click="props.toggleFullscreen"
+            class="q-ml-md"
+          ></q-btn>
+        </template>
+        
+        </q-table>
+
+
+      </div>
+
+      <div class="q-mt-md">
+        Selected: @{{ JSON.stringify(tableselected) }}
+      </div>
+
       <q-page padding class="q-pa-md">
         <p v-for="n in 5" :key="n">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nihil praesentium molestias a adipisci, dolore vitae odit, quidem consequatur optio voluptates asperiores pariatur eos numquam rerum delectus commodi perferendis voluptate?
@@ -376,12 +465,142 @@
       
       date1: '2019/02/01',
       date2: '2019-02-01 12:44:55',
+
+      tableselected: [],
+      visibleColumns: [ 'calories', 'desc', 'fat', 'carbs', 'protein', 'sodium', 'calcium', 'iron' ],
+      columns: [
+        {
+          name: 'name',
+          required: true,
+          label: 'Dessert (100g serving)',
+          align: 'left',
+          field: row => row.name,
+          format: val => `${val}`,
+          sortable: true
+        },
+        { name: 'calories', align: 'center', label: 'Calories', field: 'calories', sortable: true },
+        { name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true },
+        { name: 'carbs', label: 'Carbs (g)', field: 'carbs' },
+        { name: 'protein', label: 'Protein (g)', field: 'protein' },
+        { name: 'sodium', label: 'Sodium (mg)', field: 'sodium' },
+        { name: 'calcium', label: 'Calcium (%)', field: 'calcium', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) },
+        { name: 'iron', label: 'Iron (%)', field: 'iron', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) }
+      ],
+      data: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          sodium: 87,
+          calcium: '14%',
+          iron: '1%'
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          sodium: 129,
+          calcium: '8%',
+          iron: '1%'
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          sodium: 337,
+          calcium: '6%',
+          iron: '7%'
+        },
+        {
+          name: 'Cupcake',
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          sodium: 413,
+          calcium: '3%',
+          iron: '8%'
+        },
+        {
+          name: 'Gingerbread',
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+          sodium: 327,
+          calcium: '7%',
+          iron: '16%'
+        },
+        {
+          name: 'Jelly bean',
+          calories: 375,
+          fat: 0.0,
+          carbs: 94,
+          protein: 0.0,
+          sodium: 50,
+          calcium: '0%',
+          iron: '0%'
+        },
+        {
+          name: 'Lollipop',
+          calories: 392,
+          fat: 0.2,
+          carbs: 98,
+          protein: 0,
+          sodium: 38,
+          calcium: '0%',
+          iron: '2%'
+        },
+        {
+          name: 'Honeycomb',
+          calories: 408,
+          fat: 3.2,
+          carbs: 87,
+          protein: 6.5,
+          sodium: 562,
+          calcium: '0%',
+          iron: '45%'
+        },
+        {
+          name: 'Donut',
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+          sodium: 326,
+          calcium: '2%',
+          iron: '22%'
+        },
+        {
+          name: 'KitKat',
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          sodium: 54,
+          calcium: '12%',
+          iron: '6%'
+        }
+      ],
+
+      separator: 'cell',
+
     },
     methods: {
     
       notify: function () {
         this.$q.notify('Running on Quasar v' + this.$q.version)
-      }
+      },
+
+      getSelectedString () {
+        return this.tableselected.length === 0 ? '' : `${this.tableselected.length} record${this.tableselected.length > 1 ? 's' : ''} selected of ${this.data.length}`
+      },
       
     },
     // ...etc
