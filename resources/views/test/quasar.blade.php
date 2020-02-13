f5926d3ff2deb57f569db4383e9a5153eb427030
panxibei <panxibei@yandex.com>
2020-02-13 11:09:26 +0800

    quasar

--- resources/views/test/quasar.blade.php
+++ resources/views/test/quasar.blade.php
@@ -113,7 +113,35 @@
         </q-btn>
 
         <q-btn flat round dense icon="sim_card" class="q-mr-xs"></q-btn>
-        <q-btn flat round dense icon="gamepad"></q-btn>
+        <q-btn flat round dense icon="account_circle">
+          <q-menu>
+            <div class="row no-wrap q-pa-md">
+              <div class="column">
+                <div class="text-h6 q-mb-md">Settings</div>
+                <q-toggle v-model="mobileData" label="Use Mobile Data"></q-toggle>
+                <q-toggle v-model="bluetooth" label="Bluetooth"></q-toggle>
+              </div>
+
+              <q-separator vertical inset class="q-mx-lg"></q-separator>
+
+              <div class="column items-center">
+                <q-avatar size="72px">
+                  <img src="https://cdn.quasar.dev/img/avatar4.jpg">
+                </q-avatar>
+
+                <div class="text-subtitle1 q-mt-md q-mb-xs">John Doe</div>
+
+                <q-btn
+                  color="primary"
+                  label="Logout"
+                  push
+                  size="sm"
+                  v-close-popup
+                ></q-btn>
+              </div>
+            </div>
+          </q-menu>
+        </q-btn>
 
       </q-toolbar>
 
@@ -804,6 +832,9 @@
           ]
         }
       ],
+
+      mobileData: true,
+      bluetooth: false,
 
     },
     methods: {
