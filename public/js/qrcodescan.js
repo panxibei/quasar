096b78881f5da291f2c0d19a898b708d45ed8654
panxibei <panxibei@yandex.com>
2020-02-25 15:46:25 +0800

    quasar

--- public/js/qrcodescan.js
+++ public/js/qrcodescan.js
@@ -60,7 +60,11 @@
     }, false);
 
     startbutton.addEventListener('click', function(ev){
-      vm_app.stopscan = setInterval(takepicture, 1000);
+      vm_app.stopscan = setInterval(
+        function () {
+          takepicture();
+          vm_app.get_qrcode();
+        }, 1000);
       // alert();
       // takepicture();
       ev.preventDefault();
