8c6ae168c5e48a24ac2fddcc2d1e4d8cb661b177
panxibei <panxibei@yandex.com>
2020-02-26 14:33:30 +0800

    quasar

--- public/js/qrcodescan.js
+++ public/js/qrcodescan.js
@@ -1,128 +1,116 @@
-(function() {
-  // The width and height of the captured photo. We will set the
-  // width to the value defined here, but the height will be
-  // calculated based on the aspect ratio of the input stream.
+// 使用jsqrcode的js代码库
+// 以下为扩展调用的代码
 
-  var width = 320;    // We will scale the photo width to this
-  var height = 0;     // This will be computed based on the input stream
+var gCtx = null;
+var gCanvas = null;
 
-  // |streaming| indicates whether or not we're currently streaming
-  // video from the camera. Obviously, we start at false.
+var imageData = null;
+var ii=0;
+var jj=0;
+var c=0;
 
-  var streaming = false;
+  function dragenter(e) {
+      e.stopPropagation();
+      e.preventDefault();
+  }
 
-  // The various HTML elements we need to configure or control. These
-  // will be set by the startup() function.
+  function dragover(e) {
+      e.stopPropagation();
+      e.preventDefault();
+  }
+  function drop(e) {
+      e.stopPropagation();
+      e.preventDefault();
 
-  var startcapture = null;
-  var video = null;
-  var canvas = null;
-  var photo = null;
-  var startbutton = null;
+      var dt = e.dataTransfer;
+      var files = dt.files;
 
-  function startup() {
-	
-    video = document.getElementById('video');
-    canvas = document.getElementById('canvas');
-    photo = document.getElementById('photo');
-    startbutton = document.getElementById('startbutton');
+      handleFiles(files);
+  }
 
-    navigator.mediaDevices.getUserMedia({video: true, audio: false})
-    .then(function(stream) {
-      video.srcObject = stream;
-      video.play();
-    })
-    .catch(function(err) {
-      vm_app.$Modal.error({
-        title: '发生错误',
-        content: '未开启摄像头权限！请刷新页面后重试！'
-      });
-      console.log("An error occurred: " + err);
-    });
+  function handleFiles(f) {
+      var o=[];
+      for(var i =0;i<f.length;i++)
+      {
+      var reader = new FileReader();
 
-    video.addEventListener('canplay', function(ev){
-      if (!streaming) {
-        height = video.videoHeight / (video.videoWidth/width);
+      reader.onload = (function(theFile) {
+          return function(e) {
+          qrcode.decode(e.target.result);
+          };
+      })(f[i]);
+
+      // Read in the image file as a data URL.
+      reader.readAsDataURL(f[i]);	}
+  }
+
+  // 参数为读取的二维码字符内容
+  function read(a)
+  {
       
-        // Firefox currently has a bug where the height can't be read from
-        // the video, so we will make assumptions if this happens.
-      
-        if (isNaN(height)) {
-          height = width / (4/3);
-        }
-      
-        video.setAttribute('width', width);
-        video.setAttribute('height', height);
-        canvas.setAttribute('width', width);
-        canvas.setAttribute('height', height);
-        streaming = true;
+      if (a == 'error decoding QR Code' || a == 'Failed to load the image') {
+          return false;
+      } else {
+          
+          vm_app.close_camera();
+
+          if (vm_app.stopscan != null) {
+              clearInterval(vm_app.stopscan);
+              vm_app.stopscan = null;
+          }
+          console.log('QRCode Info: ' + a); //a 为实际二维码读取出的信息
+          vm_app.modal_qrcodescan_show = false;
+          vm_app.qrcodeinfo = a;
       }
-    }, false);
+  }	
 
-    startbutton.addEventListener('click', function(ev){
+  // function load() {
+  // 	initCanvas(640,480);
+  // 	qrcode.callback = read;
+  // 	qrcode.decode("{{ asset('statics/jsqrcode/test.jpg') }}");
+  // }
 
-      // vm_app.stopscan = setInterval(
-      vm_app.stopscan = setInterval(
-        function () {
-            takepicture();
-            vm_app.get_qrcode();
-            console.log('abcdefg');
-        }, 500);
+  // function initCanvas(ww,hh) {
+// 	gCanvas = document.getElementById("qr-canvas");
+// 	gCanvas.addEventListener("dragenter", dragenter, false);  
+// 	gCanvas.addEventListener("dragover", dragover, false);  
+// 	gCanvas.addEventListener("drop", drop, false);
+// 	var w = ww;
+// 	var h = hh;
+// 	gCanvas.style.width = w + "px";
+// 	gCanvas.style.height = h + "px";
+// 	gCanvas.width = w;
+// 	gCanvas.height = h;
+// 	gCtx = gCanvas.getContext("2d");
+// 	gCtx.clearRect(0, 0, w, h);
+// 	imageData = gCtx.getImageData( 0,0,320,240);
+// }
 
-      ev.preventDefault();
-    }, false);
-    
-    clearphoto();
-  }
+function passLine(stringPixels) { 
+  //a = (intVal >> 24) & 0xff;
 
-  // Fill the photo with an indication that none has been
-  // captured.
+  var coll = stringPixels.split("-");
 
-  function clearphoto() {
-    var context = canvas.getContext('2d');
-    context.fillStyle = "#AAA";
-    context.fillRect(0, 0, canvas.width, canvas.height);
+  for(var i=0;i<320;i++) { 
+    var intVal = parseInt(coll[i]);
+    r = (intVal >> 16) & 0xff;
+    g = (intVal >> 8) & 0xff;
+    b = (intVal ) & 0xff;
+    imageData.data[c+0]=r;
+    imageData.data[c+1]=g;
+    imageData.data[c+2]=b;
+    imageData.data[c+3]=255;
+    c+=4;
+  } 
 
-    var data = canvas.toDataURL('image/png');
-    photo.setAttribute('src', data);
-  }
-  
-  // Capture a photo by fetching the current contents of the video
-  // and drawing it into a canvas, then converting that to a PNG
-  // format data URL. By drawing it on an offscreen canvas and then
-  // drawing that to the screen, we can change its size and/or apply
-  // other changes before drawing it.
+  if(c>=320*240*4) { 
+    c=0;
+          gCtx.putImageData(imageData, 0,0);
+  } 
+ } 
 
-  function takepicture() {
-    var context = canvas.getContext('2d');
-    if (width && height) {
-      canvas.width = width;
-      canvas.height = height;
-      context.drawImage(video, 0, 0, width, height);
-    
-      var data = canvas.toDataURL('image/png');
-
-      // 获取img的src值
-      vm_app.camera_imgurl = data;
-      // console.log(vm_app.camera_imgurl);
-
-      photo.setAttribute('src', data);
-    } else {
-      clearphoto();
-    }
-  }
-
-  // Set up our event listener to run the startup process
-  // once loading is complete.
-  // window.addEventListener('load', startup, false);
-  
-// 追加代码，用于点击按钮才开始  
-  function startwebrtc() {
-	  startcapture = document.getElementById('startcapture');
-    startcapture.addEventListener('click', startup, false);
-  }
-  
-  window.addEventListener('load', startwebrtc, false);
-  
-
-})();
+  // function captureToCanvas() {
+// 	flash = document.getElementById("embedflash");
+// 	flash.ccCapture();
+// 	qrcode.decode();
+  // }
--- resources/views/test/jsqrcode.blade.php
+++ resources/views/test/jsqrcode.blade.php
@@ -69,9 +69,9 @@ fffffffffffffffffff
 <script src="{{ asset('js/axios.min.js') }}"></script>
 <script src="{{ asset('js/bluebird.min.js') }}"></script>
 <script src="{{ asset('statics/iview/iview.min.js') }}"></script>
-<script src="{{ asset('js/qrcodescan.js') }}"></script>
 <script src="{{ asset('js/httpVueLoader.js') }}"></script>
 
+<!-- 二维码扫描要加载以下js库文件 -->
 <script type="text/javascript" src="{{ asset('statics/jsqrcode/grid.js') }}"></script>
 <script type="text/javascript" src="{{ asset('statics/jsqrcode/version.js') }}"></script>
 <script type="text/javascript" src="{{ asset('statics/jsqrcode/detector.js') }}"></script>
@@ -90,121 +90,9 @@ fffffffffffffffffff
 <script type="text/javascript" src="{{ asset('statics/jsqrcode/alignpat.js') }}"></script>
 <script type="text/javascript" src="{{ asset('statics/jsqrcode/databr.js') }}"></script>
 
-<script type="text/javascript">
-    var gCtx = null;
-	var gCanvas = null;
-
-	var imageData = null;
-	var ii=0;
-	var jj=0;
-	var c=0;
-	
-    function dragenter(e) {
-        e.stopPropagation();
-        e.preventDefault();
-    }
-
-    function dragover(e) {
-        e.stopPropagation();
-        e.preventDefault();
-    }
-    function drop(e) {
-        e.stopPropagation();
-        e.preventDefault();
-
-        var dt = e.dataTransfer;
-        var files = dt.files;
-
-        handleFiles(files);
-    }
-
-    function handleFiles(f) {
-        var o=[];
-        for(var i =0;i<f.length;i++)
-        {
-        var reader = new FileReader();
-
-        reader.onload = (function(theFile) {
-            return function(e) {
-            qrcode.decode(e.target.result);
-            };
-        })(f[i]);
-
-        // Read in the image file as a data URL.
-        reader.readAsDataURL(f[i]);	}
-    }
-
-    // 参数为读取的二维码字符内容
-    function read(a)
-    {
-        
-        if (a == 'error decoding QR Code' || a == 'Failed to load the image') {
-            return false;
-        } else {
-            
-            vm_app.close_camera();
-
-            if (vm_app.stopscan != null) {
-                clearInterval(vm_app.stopscan);
-                vm_app.stopscan = null;
-            }
-            console.log('QRCode Info: ' + a); //a 为实际二维码读取出的信息
-            vm_app.modal_qrcodescan_show = false;
-            vm_app.qrcodeinfo = a;
-        }
-    }	
-	
-    // function load() {
-    // 	initCanvas(640,480);
-    // 	qrcode.callback = read;
-    // 	qrcode.decode("{{ asset('statics/jsqrcode/test.jpg') }}");
-    // }
-
-    // function initCanvas(ww,hh) {
-	// 	gCanvas = document.getElementById("qr-canvas");
-	// 	gCanvas.addEventListener("dragenter", dragenter, false);  
-	// 	gCanvas.addEventListener("dragover", dragover, false);  
-	// 	gCanvas.addEventListener("drop", drop, false);
-	// 	var w = ww;
-	// 	var h = hh;
-	// 	gCanvas.style.width = w + "px";
-	// 	gCanvas.style.height = h + "px";
-	// 	gCanvas.width = w;
-	// 	gCanvas.height = h;
-	// 	gCtx = gCanvas.getContext("2d");
-	// 	gCtx.clearRect(0, 0, w, h);
-	// 	imageData = gCtx.getImageData( 0,0,320,240);
-	// }
-
-	function passLine(stringPixels) { 
-		//a = (intVal >> 24) & 0xff;
-
-		var coll = stringPixels.split("-");
-	
-		for(var i=0;i<320;i++) { 
-			var intVal = parseInt(coll[i]);
-			r = (intVal >> 16) & 0xff;
-			g = (intVal >> 8) & 0xff;
-			b = (intVal ) & 0xff;
-			imageData.data[c+0]=r;
-			imageData.data[c+1]=g;
-			imageData.data[c+2]=b;
-			imageData.data[c+3]=255;
-			c+=4;
-		} 
-
-		if(c>=320*240*4) { 
-			c=0;
-      			gCtx.putImageData(imageData, 0,0);
-		} 
- 	} 
-
-    // function captureToCanvas() {
-	// 	flash = document.getElementById("embedflash");
-	// 	flash.ccCapture();
-	// 	qrcode.decode();
-    // }
-</script>
+<!-- 二维码扫描还要加载以下两个js文件 -->
+<script src="{{ asset('js/qrcodeCamera.js') }}"></script>
+<script src="{{ asset('js/qrcodeScan.js') }}"></script>
 
 
 <script type="text/javascript">
